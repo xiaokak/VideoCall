254fea0078b1dfc05cc9941d86fbd8a18bc18bef
毛潇恺 <31601181@stu.zucc.edu.cn>
2023-02-14 16:38:24 +0800

    2.14 视频

--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -12,6 +12,7 @@ import CallActionBox from '../../components/CallActionBox';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {Voximplant} from 'react-native-voximplant';
+import endpointEvents from 'react-native-voximplant/src/call/EndpointEvents';
 
 const permissions = [
   PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
@@ -21,6 +22,8 @@ const permissions = [
 const CallingScreen = () => {
   const [permissionGranted, setPermissionGranted] = useState(false);
   const [callStatus, setCallStatus] = useState('Initializing...');
+  const [localVideoStreamId, setLocalVideoStreamId] = useState('');
+  const [remoteVideoStreamId, setRemoteVideoStreamId] = useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -30,6 +33,7 @@ const CallingScreen = () => {
   const voximplant = Voximplant.getInstance();
 
   const call = useRef(incomingCall);
+  const endpoint = useRef(null);
 
   const goBack = () => {
     navigation.pop();
@@ -74,7 +78,12 @@ const CallingScreen = () => {
       subscribeToCallEvents();
     };
 
-    const answerCall = async () => {};
+    const answerCall = async () => {
+      subscribeToCallEvents();
+      endpoint.current = call.current.getEndpoints()[0];
+      subscribeToEndpointEvent();
+      call.current.answer(callSettings);
+    };
 
     const subscribeToCallEvents = () => {
       call.current.on(Voximplant.CallEvents.Failed, callEvent => {
@@ -90,6 +99,25 @@ const CallingScreen = () => {
       call.current.on(Voximplant.CallEvents.Disconnected, callEvent => {
         navigation.navigate('Contacts');
       });
+      call.current.on(
+        Voximplant.CallEvents.LocalVideoStreamAdded,
+        callEvent => {
+          setLocalVideoStreamId(callEvent.videoStream.id);
+        },
+      );
+      call.current.on(Voximplant.CallEvents.EndpointAdded, callEvent => {
+        endpoint.current = callEvent.endpoint;
+        subscribeToEndpointEvent();
+      });
+    };
+
+    const subscribeToEndpointEvent = async () => {
+      endpoint.current.on(
+        Voximplant.EndpointEvents.RemoteVideoStreamAdded,
+        endpointEvent => {
+          setRemoteVideoStreamId(endpointEvent.videoStream.id);
+        },
+      );
     };
 
     const showError = reason => {
@@ -124,6 +152,16 @@ const CallingScreen = () => {
       <Pressable onPress={goBack} style={styles.backButton}>
         <Ionicons name="chevron-back" color={'white'} size={25} />
       </Pressable>
+
+      <Voximplant.VideoView
+        videoStreamId={localVideoStreamId}
+        style={styles.localVideo}
+      />
+
+      <Voximplant.VideoView
+        videoStreamId={remoteVideoStreamId}
+        style={styles.remoteVideo}
+      />
 
       <View style={styles.cameraPreview}>
         <Text style={styles.name}>{user?.user_display_name}</Text>
@@ -162,6 +200,26 @@ const styles = StyleSheet.create({
     top: 50,
     left: 10,
     zIndex: 10,
+  },
+  localVideo: {
+    width: 100,
+    height: 150,
+    backgroundColor: '#ffff6e',
+
+    borderRadius: 10,
+
+    position: 'absolute',
+    right: 10,
+    top: 100,
+  },
+  remoteVideo: {
+    backgroundColor: '#7b4e80',
+    borderRadius: 10,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 100,
   },
 });
 
